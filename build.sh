ecf22be743111426557ddc56cc52c927ce5d7ac4
tolufu <darudaru0217@gmail.com>
2025-02-12 20:57:12 +0900

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py flush --noinput
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
